2555bae866ff55138e2f8c027f9ec61b8431efda
Ian Jamieson <ian.jamieson@globalgaming.com>
2021-12-01 23:06:26 +0100

    set preloaded state

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,13 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query'
+import { loadState } from '~lib/func/local-storage';
 import { messagesApi } from '~store/messages/message.service';
 
 export const store = configureStore({
   reducer: {
     [messagesApi.reducerPath]: messagesApi.reducer
   },
+  preloadedState: loadState('store'),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(messagesApi.middleware),
 });
